a5d54720379d72eaca85e22506e50c7e56656f36
singtolee <liqiang0618@outlook.com>
2018-08-31 18:03:24 +0700

    add new ms trans

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,52 @@
 import * as functions from 'firebase-functions';
+var MsTranslator = require('mstranslator');
+var client = new MsTranslator({
+    api_key: "1307c5de96104ac9b96fbb83878d4bdd"
+  }, true);
 const request = require('request');
 const API = "https://api.shenjian.io/?appid=1498f414e1d727e07af7462caf475502&scanUrl="
 export const apicall = functions.https.onRequest((req, res) => {
     const url = req.query.text;
     const enco = encodeURIComponent(url);
     const address = API.concat(enco);
+    res.set('Access-Control-Allow-Origin', "*");
+    res.set('Access-Control-Allow-Methods', 'GET, POST');
     request(address,function(err,resp,body){
         if(!err){
-            res.send(handlePrd(JSON.parse(body)))
+            handlePrd(JSON.parse(body)).then((dd)=>{
+                res.send(dd)
+            }).catch(er=>{
+                res.send(er)
+            })
         }else {
-            res.send({error:true})
             console.log('ERROR',err)
+            res.send({error:true})
         }
     })
-  //res.send("Hello from Firebase!");
  });
 
- function handlePrd(data){
+ async function handlePrd(data){
      if(data.error_code === 0){
          //successfully get prd,
          const prd = data.data.product_details;
          //gether necessary fields
-         const images = prd.images.map(item=>item.image_url);
+         const images = prd.images; //.map(item=>item.image_url);
          const name = prd.name;
-         const thName = cn2th(prd.name);
+         const thName = await callapi(prd.name); // cn2th(prd.name);
          const original_price = prd.original_price;
          const pid = prd.product_id;
          const price = prd.current_price;
          const score = prd.score;
-         const sku_detail = prd.product_sku_detail;
-         const skus = prd.sku;
+         let sku_detail = prd.product_sku_detail;
+         for (const sku of sku_detail){
+             sku.sku_thName = await callapi(sku.sku_name);
+             console.log(sku.sku_thName);
+         }
+         let skus = prd.sku;
+         for (const val of skus[0].values){
+             val.thDesc = await callapi(val.desc);
+             console.log(val.thDesc);
+         }                       //need translate
          const myPrd = {
              images:images,
              name:name,
@@ -49,6 +66,25 @@ export const apicall = functions.https.onRequest((req, res) => {
 
  }
 
- function cn2th(str){
-     return str;
- }
+ function callapi(text) {
+    return new Promise(resolve=>{
+        client.translate({text:text,from:'zh-cn',to:'th'},function(err,data){
+            if(err){
+                console.error(err)
+                //callapi(text)
+                //reject(err)
+                resolve(err)
+            }else{
+                //console.log(text)
+                //console.log(data)
+                resolve(data)
+            }
+        })
+    })
+}
+
+async function transSkus(arr){
+    const promises = arr.map(item=>callapi(item.desc));
+    await Promise.all(promises);
+}
+
